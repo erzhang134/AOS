81fa6d65ceaca5050a15fd5d9e293864ac2dfd01
erzhang134 <162892917+erzhang134@users.noreply.github.com>
2024-08-23 22:38:56 +0800

    Create 0rbit.js

---
+++ 0rbit.js
@@ -0,0 +1,11 @@
+Send({
+    Target = "QQchTCEkPhnIu9dDSFyfR9-8mG-ftAkPt7Xs3AhtBjA",
+    Action = "Build-With-0rbit",
+    Twitter = "@xxx",
+    Discord = "xxx",
+    Project_Name = "weather",
+    Project_Description = "Decentralized application of weather forecasting",
+    Project_Requirement = "Getting stock prices to build a prediction model",
+    Request_Amount = "9000000",
+    Recepient_Wallet = "xxxx"
+})
